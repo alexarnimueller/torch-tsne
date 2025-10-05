28176fbeb3f2b2635b5e8f8b1a4b494a593bd37b
Alex Müller <alexarnimueller@protonmail.com>
2025-10-05 12:03:37 +0200

    :zap: scaling

--- src/torch_tsne/main.py
+++ src/torch_tsne/main.py
@@ -33,18 +33,12 @@ def main():
         default=f"{PKG_ROOT}/examples/pytorch.png",
         help="output image path",
     )
-    parser.add_argument(
-        "-i", "--iter", type=int, help="number of iterations", default=1000
-    )
+    parser.add_argument("-i", "--iter", type=int, help="number of iterations", default=1000)
     parser.add_argument("-p", "--perplex", type=float, help="perplexity", default=30.0)
     parser.add_argument("-l", "--lr", type=float, help="learning rate", default=500.0)
-    parser.add_argument(
-        "-e", "--exager", type=float, help="early exageration", default=4.0
-    )
+    parser.add_argument("-e", "--exager", type=float, help="early exageration", default=4.0)
     parser.add_argument("-s", "--seed", type=int, help="random seed", default=42)
-    parser.add_argument(
-        "-v", "--verbose", action="store_true", help="enable verbose output"
-    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose output")
 
     args = parser.parse_args()
 
@@ -53,6 +47,9 @@ def main():
 
     # confirm that x file get same number point than label file
     assert len(X) == len(labels), "different number of datapoints and labels"
+
+    # min max normalize X to [0, 1]
+    X = (X - X.min()) / (X.max() - X.min())
 
     with torch.no_grad():
         Y = tsne(
